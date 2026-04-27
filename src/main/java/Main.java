e0725701b4438a95942d2b7273219055bd6604a4
Schönhofen Robin <s4roscho@uni-trier.de>
2026-04-27 13:04:06 +0200

    Add new Car instance to Main.java
    Added a new Car instance with color 'orange' and brand 'VW'.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
         Car car = new Car("red", "VW", 5);
         Car car1 = new Car("blue", "Audi", 3);
+        Car car2 = new Car("orange", "VW", 5);
 
         factory.buildCar(car);
         factory.buildCar(car1);
